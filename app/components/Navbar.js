4f9fa1ff4858ec880a340a10179c14778f81edb1
JBUkim <91812103@jmail.ac.kr>
2023-06-08 23:04:47 +0900

    first

---
+++ app/components/Navbar.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+export default function Layout() {
+  return (
+    <nav class="w-full py-4 bg-blue-800 shadow">
+      <div class="w-full container mx-auto flex flex-wrap items-center justify-between">
+        <nav>
+          <ul class="flex items-center justify-between font-bold text-sm text-white uppercase no-underline">
+            <li>
+              <a class="hover:text-gray-200 hover:underline px-4" href="#">
+                Shop
+              </a>
+            </li>
+            <li>
+              <a class="hover:text-gray-200 hover:underline px-4" href="#">
+                About
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <div class="flex items-center text-lg no-underline text-white pr-6">
+          <a class="" href="#">
+            <i class="fab fa-facebook">123</i>
+          </a>
+          <a class="pl-6" href="#">
+            <i class="fab fa-instagram">234</i>
+          </a>
+          <a class="pl-6" href="#">
+            <i class="fab fa-twitter">345</i>
+          </a>
+          <a class="pl-6" href="#">
+            <i class="fab fa-linkedin">456</i>
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+}
